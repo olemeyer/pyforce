b210d09c0476c9db525423c7aa31c8d87fd8105b
Ole <mail@olemeyer.com>
2020-04-14 09:28:36 +0200

    Simplifying the preparation of an OpenAI Gym

--- pyforce/env/__init__.py
+++ pyforce/env/__init__.py
@@ -1,3 +1,6 @@
 from .action_scaler import ActionSpaceScaler
 from .dict import DictEnv
 from .torch import TorchEnv
+
+def wrap_openai_gym(env):
+    return TorchEnv(ActionSpaceScaler(DictEnv(env)))
